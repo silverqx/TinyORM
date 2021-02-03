23dc6a1ad368384460e13caca2e40c2baefd1bc7
silverqx <silver.zachara@gmail.com>
2021-02-03 18:30:20 +0100

    removed debug message and forgotten file

--- tests/auto/functional/orm/tiny/basemodel/basemodel.pro
+++ tests/auto/functional/orm/tiny/basemodel/basemodel.pro
@@ -3,4 +3,3 @@ include($$TINYORM_SOURCE_TREE/tests/auto/utils.pri)
 include(../models/models.pri)
 
 SOURCES += tst_basemodel.cpp
-message(a $$DEFINES)
--- tests/auto/functional/orm/tiny/basemodel/forwards.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FORWARDS_H
-#define FORWARDS_H
-
-class Torrent;
-class TorrentPreviewableFile;
-class TorrentPeer;
-class TorrentPreviewableFileProperty;
-
-#endif // FORWARDS_H
