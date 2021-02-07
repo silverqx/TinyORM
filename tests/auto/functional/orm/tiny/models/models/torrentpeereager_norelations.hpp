49594e3cbe2d91fd5bf0a4ec71a4bfb6143efa73
silverqx <silver.zachara@gmail.com>
2021-02-07 12:05:10 +0100

    removed unnecessary relationVisitor() code

--- tests/auto/functional/orm/tiny/models/models/torrentpeereager_norelations.hpp
+++ tests/auto/functional/orm/tiny/models/models/torrentpeereager_norelations.hpp
@@ -12,11 +12,6 @@ public:
     using BaseModel::BaseModel;
 
 private:
-    /*! The visitor to obtain a type for Related template parameter. */
-    void relationVisitor(const QString &)
-    {
-    }
-
     /*! The table associated with the model. */
     QString u_table {"torrent_peers"};
 
