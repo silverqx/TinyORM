921ad9b47ada7e3c592356f2b102e4755208ec76
silverqx <silver.zachara@gmail.com>
2021-03-26 19:52:37 +0100

    added dotenv.ps1 example file
    Can be used in tests to setup up an environment.

---
+++ tests/auto/utils/testdata/dotenv.example.ps1
@@ -0,0 +1,11 @@
+# MySQL
+$Env:DB_MYSQL_HOST      = "127.0.0.1"
+$Env:DB_MYSQL_PORT      = "3306"
+$Env:DB_MYSQL_DATABASE  = ""
+$Env:DB_MYSQL_USERNAME  = ""
+$Env:DB_MYSQL_PASSWORD  = ""
+$Env:DB_MYSQL_CHARSET   = "utf8mb4"
+$Env:DB_MYSQL_COLLATION = "utf8mb4_0900_ai_ci"
+
+# SQLite
+$Env:DB_SQLITE_DATABASE = ""
