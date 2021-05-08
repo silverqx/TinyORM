7820a841bb09d81cbd2a55471f4511d183f8888b
silverqx <silver.zachara@gmail.com>
2021-05-08 09:22:05 +0200

    removed unused forward files for models

--- tests/auto/functional/orm/tiny/models/models/forwardseager.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FORWARDSEAGER_H
-#define FORWARDSEAGER_H
-
-class TorrentEager;
-class TorrentPeerEager;
-class TorrentPreviewableFileEager;
-class TorrentPreviewableFilePropertyEager;
-
-#endif // FORWARDSEAGER_H
