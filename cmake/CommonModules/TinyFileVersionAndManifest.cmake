6d5e6c6a9db5e45309baef979d497519541ebf7e
silverqx <silver.zachara@gmail.com>
2021-09-24 15:07:59 +0200

    bugfix OriginalFilename multi-config generators
    Added support for OriginalFilename in Windows RC file for multi-config
    generators.
    
     - changed InternalName in Windows RC file to TinyOrm (from tinyorm)
     - added TINY_VERSION_NUM to the version.hpp file

--- cmake/CommonModules/TinyFileVersionAndManifest.cmake
+++ cmake/CommonModules/TinyFileVersionAndManifest.cmake
@@ -1,9 +1,17 @@
 # Configure file version file and Windows resource and manifest files
 function(tiny_file_version_and_manifest target version_header_path_in
-        version_header_path_out)
+        version_header_path_out tmp_folder_path)
 
     set(tiny_original_extension)
     set(tiny_original_filename)
+    if(NOT tmp_folder_path)
+        set(tmp_folder_path "tmp/")
+    endif()
+
+    # The path is relative to the build tree, an absolute path will not be touched
+    file(REAL_PATH "${tmp_folder_path}" tmp_folder_path
+        BASE_DIRECTORY "${CMAKE_BINARY_DIR}"
+    )
 
     get_target_property(target_type ${target} TYPE)
 
@@ -17,19 +25,20 @@ function(tiny_file_version_and_manifest target version_header_path_in
         set(tiny_original_extension "${CMAKE_EXECUTABLE_SUFFIX}")
     endif()
 
-    # Append a major version number for Windows shared libraries only
-    if(${TinyOrm_target}_VERSION_EXT)
-        set(tiny_original_filename
-            "${target}${PROJECT_VERSION_MAJOR}${tiny_original_extension}")
-    else()
-        set(tiny_original_filename
-            "${target}${tiny_original_extension}")
-    endif()
+    # OriginalFilename behavior:
+    # - append a major version number for Windows shared libraries only
+    # - append d for Debug configuration for shared and static library
 
     configure_file(
         "${version_header_path_in}version.hpp.in"
-        "${version_header_path_out}version.hpp"
+        "${tmp_folder_path}version.hpp.in"
         @ONLY NEWLINE_STYLE LF
+    )
+
+    # To support an OriginalFilename in Windows RC file for multi-config generators
+    file(GENERATE OUTPUT "${version_header_path_out}version.hpp"
+        INPUT "${CMAKE_BINARY_DIR}/${tmp_folder_path}version.hpp.in"
+        NEWLINE_STYLE UNIX
     )
 
     target_sources(${target} PRIVATE
