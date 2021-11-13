7b6c4e6ffeb9f5768d585595a58865c3feb061b7
silverqx <silver.zachara@gmail.com>
2021-11-13 11:11:11 +0100

    PS script to detect dll export in header files

---
+++ tools/ExportInHeader.ps1
@@ -0,0 +1,26 @@
+$hppFiles = Get-ChildItem -Path ..\include\*.hpp -Recurse
+$cppFiles = Get-ChildItem -Path ..\src\*.cpp -Recurse
+
+foreach ($hppFile in $hppFiles)
+{
+    $cppFile = $hppFile -replace '\\include\\', '\src\'
+    $cppFile = $cppFile -replace '\.hpp', '.cpp'
+
+    $hasCpp = $cppFiles | Where-Object FullName -CEQ $cppFile
+    if ($hasCpp.Count -gt 1)
+    {
+        throw 'More results for ' + $cppFile
+    }
+    if ($hasCpp.Count -eq 1)
+    {
+        continue
+    }
+
+    $containsExport = Select-String -Path $hppFile -Pattern 'SHAREDLIB_EXPORT'
+    if ($containsExport.Count -eq 0)
+    {
+        continue
+    }
+
+    Write-Output $hppFile.FullName
+}
