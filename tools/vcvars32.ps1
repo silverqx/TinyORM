35cf938440a0280b574afcda92ddb47026cdf518
silverqx <silver.zachara@gmail.com>
2021-12-27 16:53:01 +0100

    sync docs, added Building and Hello world docs
    RootFolderInput component allows to input the root folder and
    application folder and these two folders are used across whole
    documentation, they are persisted in the local storage.
    
    Also pretty huge enhancements:
    
     - Docusaurus upgraded to 2.0.0-beta.14
     - APITable component
     - RootFolderInput / RootFolder components
     - enhanced docusaurus.config.js
     - new qmake syntax highlighter
     - fixed bash syntax highlighter
     - fixed robots.txt, moved to static/ folder
     - enabled sass for css modules and for global customCss

---
+++ tools/vcvars32.ps1
@@ -0,0 +1,19 @@
+Param(
+    [Parameter(Position = 0, HelpMessage = 'Visual Studio version.')]
+    [ValidateNotNull()]
+    [ValidatePattern('2019|2022')]
+    [Int]$Version = 2019
+)
+
+Set-StrictMode -Version 3.0
+
+$programFiles = $Version -gt 2019 ? 'Program Files' : 'Program Files (x86)'
+
+cmd.exe /c "call `"C:\$programFiles\Microsoft Visual Studio\$Version\Community\VC\Auxiliary\Build\vcvars32.bat`" && set > %TEMP%\vcvars32_$Version.tmp"
+
+Get-Content "$env:TEMP\vcvars32_$Version.tmp" | Foreach-Object {
+    if ($_ -match "^(.*?)=(.*)$") {
+        Set-Content "env:\$($matches[1])" $matches[2]
+    }
+}
+
