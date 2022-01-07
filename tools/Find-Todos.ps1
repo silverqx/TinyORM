bc5e987ef19fbceba23ee4824b569eecf8fd7acd
silverqx <silver.zachara@gmail.com>
2022-01-07 09:11:24 +0100

    added new TMP todo keyword to Find-Todos.ps1

--- tools/Find-Todos.ps1
+++ tools/Find-Todos.ps1
@@ -19,7 +19,7 @@ Param(
 
 Set-StrictMode -Version 3.0
 
-$Script:TodoKeywordsPattern = ' (TODO|NOTE|FIXME|BUG|WARNING|CUR|FEATURE|TEST|FUTURE|CUR1) '
+$Script:TodoKeywordsPattern = ' (TODO|NOTE|FIXME|BUG|WARNING|CUR|FEATURE|TEST|FUTURE|CUR1|TMP) '
 
 Get-ChildItem -Path $Path -Include $Include -Recurse | `
     Select-String -Pattern $Script:TodoKeywordsPattern -CaseSensitive | `
