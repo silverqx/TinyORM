d2bd40988d86e48cb897f5a80854bbfe5c3057de
silverqx <silver.zachara@gmail.com>
2022-03-25 16:53:41 +0100

    bugfix mysql.h include
    Switched __has_include() condition, moved mysql/mysql.h higher to avoid
    warnings from mysql.h with -isystem includes.

--- src/orm/mysqlconnection.cpp
+++ src/orm/mysqlconnection.cpp
@@ -9,10 +9,10 @@
 #  ifdef __MINGW32__
 #    include <mysql/errmsg.h>
 #  endif
-#  if __has_include(<mysql.h>)
-#    include <mysql.h>
-#  elif __has_include(<mysql/mysql.h>)
+#  if __has_include(<mysql/mysql.h>)
 #    include <mysql/mysql.h>
+#  elif __has_include(<mysql.h>)
+#    include <mysql.h>
 #  else
 #    error Can not find <mysql.h> header file, install the MySQL C client library or \
 disable TINYORM_MYSQL_PING preprocessor directive.
