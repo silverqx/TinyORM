b52bdab78c1ff2f9f6b6674ef5a71d94aa918aa7
silverqx <silver.zachara@gmail.com>
2022-06-15 22:51:45 +0200

    fixed clazy warning

--- tom/src/tom/commands/integratecommand.cpp
+++ tom/src/tom/commands/integratecommand.cpp
@@ -416,7 +416,7 @@ bool IntegrateCommand::isZshCompletionRegistered()
 
 bool IntegrateCommand::writeTomZshCompletionToExistFolder()
 {
-    for (const auto &completionFilepath : *TomZshCompletionPaths)
+    for (const auto &completionFilepath : std::as_const(*TomZshCompletionPaths))
         if (QDir(completionFilepath.dirPath).exists() &&
             writeTomZshCompletion(completionFilepath.filePath)
         )
