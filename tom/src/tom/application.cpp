2c875bddb16c150cc9bdf92c5fb34b5bf0bc7194
silverqx <silver.zachara@gmail.com>
2022-07-03 16:15:31 +0200

    updated todo task

--- tom/src/tom/application.cpp
+++ tom/src/tom/application.cpp
@@ -678,4 +678,4 @@ int Application::runWithArguments(QStringList &&arguments)
 
 TINYORM_END_COMMON_NAMESPACE
 
-// CUR tom, commands I want to implement; make:model, test, schema:dump silverqx
+// CUR tom, commands I want to implement; test, schema:dump silverqx
