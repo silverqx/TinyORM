b9b0a7f96d64e288cecd8033c1ad389d4ec4400a
silverqx <silver.zachara@gmail.com>
2022-08-04 17:48:45 +0200

    updated todo task

--- src/orm/tiny/types/syncchanges.cpp
+++ src/orm/tiny/types/syncchanges.cpp
@@ -15,7 +15,7 @@ SyncChanges::SyncChanges()
 
 bool SyncChanges::isValidKey(const QString &key)
 {
-    // FUTURE simply use contains(key) and remove syncKeys(), but this needs to = delete all methods that can modify this map as it's derived from the std::map because now an user can simply add another xyz key to the map and this method would return incorrect results silverqx
+    // FUTURE simply use contains(key) and remove syncKeys(), but this needs to = delete all methods that can modify this map as it's derived from the std::map because now an user can simply add another xyz key to the map and this method would return incorrect results, even better will be to define std::map as data member and not deriving from it silverqx
     return syncKeys().contains(key);
 }
 
