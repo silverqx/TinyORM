be1fab62fcc9290ba9c82391782f13076b2f7965
silverqx <silver.zachara@gmail.com>
2022-12-04 15:03:51 +0100

    used std::move()

--- include/orm/utils/query.hpp
+++ include/orm/utils/query.hpp
@@ -104,7 +104,7 @@ namespace Orm::Utils
             queryString.replace(queryString.indexOf(QLatin1Char('?')), 1, bindingValue);
 
             if (simpleBindings)
-                simpleBindingsList << bindingValue;
+                simpleBindingsList << std::move(bindingValue);
         }
 
         return {std::move(queryString), std::move(simpleBindingsList)};
