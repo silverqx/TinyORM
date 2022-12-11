4ecf4990294411fae4e79ed29d8ad9af63e17d4f
silverqx <silver.zachara@gmail.com>
2022-12-11 20:07:47 +0100

    enhanced constrained()
     - execute the guess table logic only if the table is empty
     - return pluralized foreign key name if the column is empty

--- src/orm/schema/foreignidcolumndefinitionreference.cpp
+++ src/orm/schema/foreignidcolumndefinitionreference.cpp
@@ -20,15 +20,22 @@ ForeignIdColumnDefinitionReference::constrained(const QString &table,
                                                 const QString &column)
 {
     // Guess table name, cut off _column from the foreign index name and make it plural
-    const auto &foreignName = m_columnDefinition.get().name;
+    const auto guessTableName = [this, &column]
+    {
+        const auto &foreignName = m_columnDefinition.get().name;
 
-    const auto guessTable = QStringLiteral("%1s")
-                            .arg(foreignName.left(
-                                     foreignName.lastIndexOf(
-                                         QStringLiteral("_%1").arg(column))));
+        // Nothing to guess, the column is empty
+        if (column.isEmpty())
+            return TMPL_PLURAL.arg(foreignName);
+
+        // Don's use the Qt6's QString::first() here (the result can be -1)
+        return TMPL_PLURAL.arg(foreignName.left(
+                                   foreignName.lastIndexOf(
+                                       QStringLiteral("_%1").arg(column))));
+    };
 
     return references(QVector<QString> {column})
-            .on(table.isEmpty() ? guessTable : table);
+            .on(table.isEmpty() ? guessTableName() : table);
 }
 
 ForeignKeyDefinitionReference
