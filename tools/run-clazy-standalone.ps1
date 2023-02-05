af0a4b386b80fe72d09dc15e888336a6825ca8dd
silverqx <silver.zachara@gmail.com>
2023-02-05 09:11:42 +0100

    tools added comment
    [skip ci]

--- tools/run-clazy-standalone.ps1
+++ tools/run-clazy-standalone.ps1
@@ -6,4 +6,6 @@ if (-not (Test-Path .\compile_commands.json)) {
     throw 'Compile database compile_commands.json doesn''t exist.'
 }
 
+# The run-clazy-standalone python script is in the same folder as this script, copy it where ever
+# you want and update the path to it below.
 python.exe '<your_path_here>\dotfiles\python\run-clazy-standalone.py' @args
