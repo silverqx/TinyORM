a036e3d1579df15a79295da9e714fa82bf9c1909
silverqx <silver.zachara@gmail.com>
2023-02-08 15:38:03 +0100

    added IWYU pragma

--- include/orm/schema/schemaconstants.hpp
+++ include/orm/schema/schemaconstants.hpp
@@ -8,9 +8,9 @@ TINY_SYSTEM_HEADER
 #include "orm/config.hpp"
 
 #ifdef TINYORM_EXTERN_CONSTANTS
-#  include "orm/schema/schemaconstants_extern.hpp"
+#  include "orm/schema/schemaconstants_extern.hpp" // IWYU pragma: export
 #else
-#  include "orm/schema/schemaconstants_inline.hpp"
+#  include "orm/schema/schemaconstants_inline.hpp" // IWYU pragma: export
 #endif
 
 #endif // ORM_SCHEMA_SCHEMACONSTANTS_HPP
