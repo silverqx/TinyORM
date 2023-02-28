df29453e6840ade4b0a6ad50b5d2a1940471363b
silverqx <silver.zachara@gmail.com>
2023-02-28 13:32:55 +0100

    constants fixed order

--- include/orm/constants_extern.hpp
+++ include/orm/constants_extern.hpp
@@ -136,9 +136,9 @@ namespace Orm::Constants
     SHAREDLIB_EXPORT extern const QString MyISAM;
     SHAREDLIB_EXPORT extern const QString postgres_;
 
-    SHAREDLIB_EXPORT extern const QString UTF8MB40900aici;
     SHAREDLIB_EXPORT extern const QString UTF8Generalci;
     SHAREDLIB_EXPORT extern const QString UTF8Unicodeci;
+    SHAREDLIB_EXPORT extern const QString UTF8MB40900aici;
     SHAREDLIB_EXPORT extern const QString UcsBasic;
     SHAREDLIB_EXPORT extern const QString en_US;
     SHAREDLIB_EXPORT extern const QString POSIX_;
