bc1ba5cf2292c44d68e51be21b88a0f3e2574111
silverqx <silver.zachara@gmail.com>
2023-04-01 10:16:24 +0200

    bugfix removed useless const

--- include/orm/types/sqlquery.hpp
+++ include/orm/types/sqlquery.hpp
@@ -48,7 +48,7 @@ namespace Types
         /*! Constructor from the QSqlQuery type and time zone from the configuration. */
         SqlQuery(QSqlQuery &&other, const QtTimeZoneConfig &qtTimeZone,
                  const QueryGrammar &queryGrammar,
-                 const std::optional<bool> returnQDateTime);
+                 std::optional<bool> returnQDateTime);
 
         /*! Return the value of field index in the current record. */
         inline QVariant value(int index) const;
