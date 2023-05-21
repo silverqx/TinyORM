be613b6bcd069398992154dcc8e34b5ba19a7821
silverqx <silver.zachara@gmail.com>
2023-05-21 11:36:26 +0200

    removed useless T_THREAD_LOCAL

--- include/orm/tiny/concerns/hasrelationstore.hpp
+++ include/orm/tiny/concerns/hasrelationstore.hpp
@@ -263,8 +263,7 @@ namespace Orm::Tiny::Concerns
             constexpr static RelationStoreType initStoreType();
 
             /*! Served store type, this class can handle two store types. */
-            T_THREAD_LOCAL
-            constexpr static const RelationStoreType STORE_TYPE = initStoreType();
+            constexpr static const RelationStoreType STORE_TYPE = initStoreType(); // thread_local not needed
 
         private:
             /*! The QueriesRelationships instance to which the visited relation will be
