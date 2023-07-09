7b232992a83833a42a842e4aa7ab6fb605a7e0c9
silverqx <silver.zachara@gmail.com>
2023-07-09 15:07:16 +0200

    changed exception message

--- src/orm/tiny/exceptions/relationnotfounderror.cpp
+++ src/orm/tiny/exceptions/relationnotfounderror.cpp
@@ -45,8 +45,8 @@ QString RelationNotFoundError::formatMessage(const QString &model,
 
     default:
         return QStringLiteral(
-                    "Call to undefined relationship '%1' (in the u_relation data "
-                    "member) on model '%2'.")
+                    "The relationship mapping '%1' doesn't exist in the u_relations map "
+                    "data member for the '%2' model.")
                 .arg(relation, model);
     }
 }
