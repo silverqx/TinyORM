ab7ef8ec39edd8c0d24ee9080d2fb9f54d516a9e
silverqx <silver.zachara@gmail.com>
2023-08-12 18:15:06 +0200

    added comment

--- include/orm/macros/systemheader.hpp
+++ include/orm/macros/systemheader.hpp
@@ -2,6 +2,8 @@
 #ifndef ORM_MACROS_SYSTEMHEADER_HPP
 #define ORM_MACROS_SYSTEMHEADER_HPP
 
+/* The system_header pragma is disabled if building the TinyORM library itself
+   as the shared library. */
 #if !defined(TINYORM_PRAGMA_SYSTEM_HEADER_OFF) && !defined(TINYORM_BUILDING_SHARED)
 // Clang masquerades as GCC 4.2.0 so it has to be first
 #  ifdef __clang__
