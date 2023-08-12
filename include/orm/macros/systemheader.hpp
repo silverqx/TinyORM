f2ba1c472e0b7898c1cb0b1678b135caec94d9e4
silverqx <silver.zachara@gmail.com>
2023-08-12 18:14:42 +0200

    used #ifdef and #ifndef

--- include/orm/macros/systemheader.hpp
+++ include/orm/macros/systemheader.hpp
@@ -4,7 +4,7 @@
 
 #if !defined(TINYORM_PRAGMA_SYSTEM_HEADER_OFF) && !defined(TINYORM_BUILDING_SHARED)
 // Clang masquerades as GCC 4.2.0 so it has to be first
-#  if defined(__clang__)
+#  ifdef __clang__
 #    define TINY_SYSTEM_HEADER _Pragma("clang system_header")
 #  elif __GNUC__ * 100 + __GNUC_MINOR__ > 301
 #    define TINY_SYSTEM_HEADER _Pragma("GCC system_header")
@@ -13,7 +13,7 @@
 #  endif
 #endif
 
-#if !defined(TINY_SYSTEM_HEADER)
+#ifndef TINY_SYSTEM_HEADER
 #  define TINY_SYSTEM_HEADER
 #endif
 
