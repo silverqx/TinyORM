c09dd85b11d36789572aeb4edd866af91b944edc
silverqx <silver.zachara@gmail.com>
2023-09-17 13:21:53 +0200

    tools common finished Common-Path.ps1

--- tools/private/Common-Path.ps1
+++ tools/private/Common-Path.ps1
@@ -2,14 +2,23 @@
 
 Set-StrictMode -Version 3.0
 
-# Get slashes by platform
+# Get slashes by platform (all allowed platform dependant path separators)
 function Get-Slashes {
     [OutputType([string])]
     Param()
 
-    # Cached value
-    if ($Script:Slashes) {
-        return $Script:Slashes
+    # Test precondition only if the $Script:Slashes is defined
+    if (Test-Path 'Variable:Script:Slashes') {
+        # Test correct type
+        if ($null -ne $Script:Slashes -and $Script:Slashes -isnot [string]) {
+            throw 'The $Script:Slashes variable must be [string] or $null, the current type is [' +
+                $Script:Slashes.GetType() + '].'
+        }
+
+        # Cached value
+        if (-not [string]::IsNullOrEmpty($Script:Slashes)) {
+            return $Script:Slashes
+        }
     }
 
     $platform = $PSVersionTable.Platform
@@ -27,11 +36,19 @@ function Get-Slashes {
     }
 }
 
+# Get the RegEx for a given path that can be used to determine if the given path is
+# on the system path ($env:Path or $PATH).
+# It can also be used to match only one path because the RegEx starts/ends like this:
+# (?:^|;)...(?:$|;)
+# It's cross-platform. It doesn't normalize the driver letter on Windows,
+# it normalizes path slashes only.
+# Generates the RegEx something like the following:
+# (?:^|;)E:[\\/]+dotfiles[\\/]+bin[\\/]*(?:$|;)
 function Get-PathToMatch {
     [OutputType([string])]
     Param(
         [Parameter(Position = 0, Mandatory, ValueFromPipeline, ValueFromPipelinebyPropertyName,
-            HelpMessage = "Specifies a path to modify to the match pattern.")]
+            HelpMessage = 'Specifies the path for which the match pattern is to be returned.')]
         [ValidateNotNullOrEmpty()]
         [string]
         $Path
@@ -39,6 +56,7 @@ function Get-PathToMatch {
 
     $slashes = Get-Slashes
 
+    # Normalize path slashes
     $pathToMatch = $Path `
         -replace "[$slashes]+(?![$slashes]*$)", "[$slashes]+" `
         -replace "[$slashes]+$", ''
@@ -48,6 +66,7 @@ function Get-PathToMatch {
     return "(?:^|$separator)$pathToMatch[$slashes]*(?:$|$separator)"
 }
 
+# Remove a path separator at the end and call the [System.IO.Path]::GetFullPath
 function Get-FullPath {
     [CmdletBinding()]
     [OutputType([string[]])]
@@ -62,7 +81,9 @@ function Get-FullPath {
         [string[]] $normalized = @()
 
         foreach ($p in $Path) {
-            $normalized += [System.IO.Path]::GetFullPath(($p -replace "[$(Get-Slashes)]+$", ''))
+            $normalized += [System.IO.Path]::GetFullPath(
+                ($p -replace "[$(Get-Slashes)]+$", '')
+            )
         }
 
         return $normalized
