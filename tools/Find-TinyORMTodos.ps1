15a87422488b0770f4738cc101ab8b4a7f1b5883
silverqx <silver.zachara@gmail.com>
2023-10-03 16:36:33 +0200

    tools added find todos shortcuts for TinyORM

---
+++ tools/Find-TinyORMTodos.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/env pwsh
+
+Param(
+    [Parameter(Position = 0, Mandatory,
+        HelpMessage = 'Specifies todo tasks to find on each line. The pattern value is used '+
+            'in regular expression.')]
+    [ValidateNotNullOrEmpty()]
+    [string] $Pattern,
+
+    [Parameter(Position = 1, HelpMessage = 'Todo keywords regex pattern.')]
+    [ValidateNotNullOrEmpty()]
+    [string] $TodoKeywordsPattern = ' (TODO|NOTE|FIXME|BUG|WARNING|CUR|FEATURE|TEST|FUTURE|CUR1|TMP|SEC) '
+)
+
+Set-StrictMode -Version 3.0
+
+E:\dotfiles\bin\Find-Todos.ps1 -Path 'E:\c\qMedia\TinyORM\TinyORM' -Include *.cpp, *.hpp `
+    -InSubFoldersPattern '(include|src|tests|tom)' @PSBoundParameters | Format-List
+    # | Format-List -Property @{
+    #     Name       = 'Line'
+    #     Expression = {
+    #         "`e[33m$($_.Line)`e[0m"
+    #     }
+    # }, LineNumber, Path
---
+++ tools/qft.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/env pwsh
+
+Param(
+    [Parameter(Position = 0, Mandatory,
+        HelpMessage = 'Specifies todo tasks to find on each line. The pattern value is used ' +
+            'in regular expression.')]
+    [ValidateNotNullOrEmpty()]
+    [string] $Pattern,
+
+    [Parameter(Position = 1, HelpMessage = 'Todo keywords regex pattern.')]
+    [ValidateNotNullOrEmpty()]
+    [string] $TodoKeywordsPattern = ' (TODO|NOTE|FIXME|BUG|WARNING|CUR|FEATURE|TEST|FUTURE|CUR1|TMP|SEC) '
+)
+
+Set-StrictMode -Version 3.0
+
+E:\dotfiles\bin\Find-TinyORMTodos.ps1 @PSBoundParameters
