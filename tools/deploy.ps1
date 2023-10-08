128afa6b896026999b1d4f5c5016192ec421774f
silverqx <silver.zachara@gmail.com>
2023-10-08 15:43:00 +0200

    docs updated tom CMake project

--- tools/deploy.ps1
+++ tools/deploy.ps1
@@ -87,7 +87,7 @@ function Initialize-ScriptVariables {
                 (Resolve-Path -Path ./cmake/vcpkg/ports/tinyorm/vcpkg.json)     = 1
                 (Resolve-Path -Path ./cmake/vcpkg/ports/tinyorm-qt5/vcpkg.json) = 1
                 (Resolve-Path -Path ./docs/building/hello-world.mdx)            = 4
-                (Resolve-Path -Path ./docs/building/migrations.mdx)             = 2
+                (Resolve-Path -Path ./docs/building/migrations.mdx)             = 1
                 (Resolve-Path -Path ./docs/building/tinyorm.mdx)                = 1
             }
             [VersionType]::VersionWith_v = [ordered] @{
