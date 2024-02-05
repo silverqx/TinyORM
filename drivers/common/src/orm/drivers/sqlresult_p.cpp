5e002fd0c81adc7a822e917bc2a77e2a4d6d805f
silverqx <silver.zachara@gmail.com>
2024-02-05 14:28:19 +0100

    drivers added comments

--- drivers/common/src/orm/drivers/sqlresult_p.cpp
+++ drivers/common/src/orm/drivers/sqlresult_p.cpp
@@ -19,6 +19,27 @@ SqlResultPrivate::SqlResultPrivate(const std::weak_ptr<SqlDriver> &driver) noexc
     Q_ASSERT(!sqldriver.expired());
 }
 
+/* This problematic is much more complex. Qt can represent much higher QDate ranges
+   than the MySQL server and currently, TinyDrivers doesn't check these ranges that means
+   we depend on error codes returned from the DB server.
+
+   MySQL ranges:
+    - DATE: '1000-01-01' to '9999-12-31'
+    - TIME: '-838:59:59.000000' to '838:59:59.000000'
+
+   Qt ranges, it depends whether Daylight-Saving Time (DST) is used, the ranges are huge
+   without the DST and with DST they are around 1970-01-01 to 2037-12-31.
+
+   The QDate is OK there is nothing wrong with it.
+
+   But the QTime isn't good, Qt can't represent QTime outside of the following ranges:
+   Hours 0-23, minutes/seconds 0-59, and 0-999 for milliseconds.
+
+   The other thing is that invalid QDateTime and QTime has an empty ISODate so send it
+   as the NULL value instead of an empty string.
+   I don't like this but we need to be API compatible with QtSql module so I leave this
+   logic untouched. */
+
 bool SqlResultPrivate::isVariantNull(const QVariant &value)
 {
     // This method is used in prepared bindings only
@@ -31,15 +52,16 @@ bool SqlResultPrivate::isVariantNull(const QVariant &value)
         return static_cast<const QString *>(value.constData())->isNull();
 
     case qMetaTypeId<QDateTime>():
-        // CUR drivers check this if I send invalid date to the DB what happens, I think that problem will be in this and then update the comment appropriately, the same for the QTime few lines below silverqx
-        // We treat invalid date-time as null, since its ISODate would be empty.
+        /* Invalid QDateTime has an empty ISODate so send it as the NULL value instead
+           of an empty string. */
         return !static_cast<const QDateTime *>(value.constData())->isValid();
 
     case qMetaTypeId<QDate>():
         return static_cast<const QDate *>(value.constData())->isNull();
 
     case qMetaTypeId<QTime>():
-        // As for QDateTime, QTime can be invalid without being null.
+        /* Invalid QTime has an empty ISODate so send it as the NULL value instead
+           of an empty string. */
         return !static_cast<const QTime *>(value.constData())->isValid();
 
     case qMetaTypeId<QByteArray>():
