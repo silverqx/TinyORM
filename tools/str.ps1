6040f92b9b39d3e2e5bcb9dfac44cbbcc12f13fe
silverqx <silver.zachara@gmail.com>
2024-02-11 19:04:01 +0100

    tools added source and deploy pwsh scripts

---
+++ tools/str.ps1
@@ -0,0 +1,14 @@
+#!/usr/bin/env pwsh
+
+Param(
+    [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelinebyPropertyName,
+        HelpMessage = 'Specifies the TinyORM release build folder to add.')]
+    [ValidateNotNullOrEmpty()]
+    [string[]] $BuildPath = $($(Get-Location).Path)
+)
+
+Set-StrictMode -Version 3.0
+
+Add-FolderOnPath.ps1 -Path $BuildPath\src\release,
+                           $BuildPath\examples\tom\release,
+                           $BuildPath\tests\testdata_tom\release
