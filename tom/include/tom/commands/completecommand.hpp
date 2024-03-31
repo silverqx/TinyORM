07937a44769ce568c5b1147f426a8e36e4e2c102
silverqx <silver.zachara@gmail.com>
2024-03-31 21:27:22 +0200

    tom fixtypo

--- tom/include/tom/commands/completecommand.hpp
+++ tom/include/tom/commands/completecommand.hpp
@@ -89,7 +89,7 @@ namespace Tom::Commands
         inline static bool isLongOption(const QString &wordArg);
         /*! Determine whether the given word is a short option argument. */
         inline static bool isShortOption(const QString &wordArg);
-        /*! Get the command-line option value (--database=value). */
+        /*! Get the command-line option value (eg. --database=value). */
         static QString getOptionValue(const QString &wordArg);
 
         /*! Determine whether the given command contains the given option. */
