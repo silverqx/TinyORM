c32ec5a2e678328b1c6d52fcb0f260640081634c
silverqx <silver.zachara@gmail.com>
2024-06-19 12:12:19 +0200

    upgraded to Qt v6.7.2
     - docs
     - tools
     - workflows
     - NOTES.txt

--- tom/src/tom/commands/aboutcommand.cpp
+++ tom/src/tom/commands/aboutcommand.cpp
@@ -249,17 +249,17 @@ AboutCommand::printAboutItemsDetail(const std::map<QString, AboutValue> &aboutIt
 /* I won't serialize the Dependencies components into the JSON output because that
    would add unnecessary complexity to the consumers' parsing code.
    Currently the output looks like:
-"dependencies": {"qt": "6.7.1", "range-v3": "0.12.0", "tinydrivers": "0.1.1"}
+"dependencies": {"qt": "6.7.2", "range-v3": "0.12.0", "tinydrivers": "0.1.1"}
 
    With the Dependencies' components it would look like:
 "dependencies": {
     "qt": {
-        "version": "6.7.1",
+        "version": "6.7.2",
         "components": ["Core"]
     },
     "range-v3": "0.12.0",
     "tinydrivers": : {
-        "version": "6.7.1",
+        "version": "6.7.2",
         "components": ["MySQL"]
     }
 }
