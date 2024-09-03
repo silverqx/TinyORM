9ecde54200c6ef20d861379afc9ab3b6961e3bb9
silverqx <silver.zachara@gmail.com>
2024-09-03 14:52:53 +0200

    msvc workaround
    The using Qt::StringLiterals::operator""_s; doesn't work here in all
    cases.

--- include/orm/support/replacebindings.hpp
+++ include/orm/support/replacebindings.hpp
@@ -44,7 +44,7 @@ namespace Orm::Support
     ReplaceBindings::replaceBindingsInSql(QString queryString, const T &bindings,
                                           const bool simpleBindings)
     {
-        using Qt::StringLiterals::operator""_s;
+        using namespace Qt::StringLiterals;
 
         // Can't use orm/constants.hpp because this class will be also used in TinyDrivers
         static const auto Invalid     = u"INVALID"_s;
