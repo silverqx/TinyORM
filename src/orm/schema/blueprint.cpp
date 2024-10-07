c9e77f701ad70c101d5577391d8a64288fa0ff83
silverqx <silver.zachara@gmail.com>
2024-10-07 20:42:02 +0200

    schema used auto

--- src/orm/schema/blueprint.cpp
+++ src/orm/schema/blueprint.cpp
@@ -625,7 +625,7 @@ void Blueprint::addFluentIndexes()
     for (auto &column : m_columns)
         /* Loop over all column's index data members to check if any index has been
            defined. */
-        for (const std::array indexes {
+        for (const auto indexes {
                  std::to_array<FluentIndexItem>({
                      {std::ref(column.index),        Index},
                      {std::ref(column.primary),      Primary},
