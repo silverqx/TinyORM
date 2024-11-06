cf6c3b54f42194dd9da615872e6e760acfd25afe
silverqx <silver.zachara@gmail.com>
2024-11-06 16:06:21 +0100

    schema fixtypo

--- include/orm/schema/grammars/postgresschemagrammar.hpp
+++ include/orm/schema/grammars/postgresschemagrammar.hpp
@@ -320,7 +320,7 @@ namespace Grammars
             nameList.reserve(name.count(DOT));
 
             for (const auto segment : QStringView(name).split(DOT, Qt::KeepEmptyParts))
-                // Don't use the TRIM_QUOTES here to avoid include of schema constants
+                // Don't use the TRIM_QUOTES here to avoid #include of schema constants
                 nameList << StringUtils::trim(segment, uR"('")"_s).toString();
 
             return TMPL_DQUOTES.arg(nameList.join(uR"(".")"_s));
