cc82b9b9025d01f77d9c48ccb615e89cae090001
silverqx <silver.zachara@gmail.com>
2024-11-11 11:43:11 +0100

    schema whitespace

--- src/orm/schema/schemabuilder.cpp
+++ src/orm/schema/schemabuilder.cpp
@@ -44,8 +44,7 @@ SchemaBuilder::dropDatabaseIfExists(const QString &/*unused*/) const
 void SchemaBuilder::create(const QString &table,
                            const std::function<void(Blueprint &)> &callback) const
 {
-    build(Helpers::tap<Blueprint>(createBlueprint(table),
-                                  [&callback](auto &blueprint)
+    build(Helpers::tap<Blueprint>(createBlueprint(table), [&callback](auto &blueprint)
     {
         blueprint.create();
 
