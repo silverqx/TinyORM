9468491327bd967c23d418f82abfb09c1761baf8
silverqx <silver.zachara@gmail.com>
2024-12-01 13:18:18 +0100

    cmake added comments

--- cmake/Modules/TinySources.cmake
+++ cmake/Modules/TinySources.cmake
@@ -167,7 +167,7 @@ function(tinymysql_sources out_headers_private out_headers out_sources) #[[overr
     list(SORT headers)
     list(SORT sources)
 
-    set(sourceDir "${${TinyOrm_ns}_SOURCE_DIR}/drivers/mysql")
+    set(sourceDir "${${TinyOrm_ns}_SOURCE_DIR}/drivers/mysql") # Absolute path needed here
     set(suffixDir "orm/drivers/mysql")
 
     list(TRANSFORM headers_private PREPEND "${sourceDir}/include_private/${suffixDir}/")
@@ -700,7 +700,9 @@ function(tiny_model_sources out_headers out_sources)
 
     list(SORT headers)
 
-    list(TRANSFORM headers PREPEND "${${TinyOrm_ns}_SOURCE_DIR}/tests/models/models/")
+    list(TRANSFORM headers
+        PREPEND "${${TinyOrm_ns}_SOURCE_DIR}/tests/models/models/" # Absolute path needed here
+    )
 
     set(${out_headers} ${headers} PARENT_SCOPE)
     set(${out_sources} ${sources} PARENT_SCOPE)
@@ -724,7 +726,7 @@ function(tiny_tests_migration_sources out_headers)
     list(SORT headers)
 
     list(TRANSFORM headers
-        PREPEND "${${TinyOrm_ns}_SOURCE_DIR}/tests/database/migrations/"
+        PREPEND "${${TinyOrm_ns}_SOURCE_DIR}/tests/database/migrations/" # Absolute path needed here
     )
 
     set(${out_headers} ${headers} PARENT_SCOPE)
@@ -746,7 +748,9 @@ function(tiny_tests_seeder_sources out_headers)
 
     list(SORT headers)
 
-    list(TRANSFORM headers PREPEND "${${TinyOrm_ns}_SOURCE_DIR}/tests/database/seeders/")
+    list(TRANSFORM headers
+        PREPEND "${${TinyOrm_ns}_SOURCE_DIR}/tests/database/seeders/" # Absolute path needed here
+    )
 
     set(${out_headers} ${headers} PARENT_SCOPE)
 
