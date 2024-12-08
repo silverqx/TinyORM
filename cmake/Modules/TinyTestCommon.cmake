30dfaa8a4ae2d6a10671d5439dda61560e137e00
silverqx <silver.zachara@gmail.com>
2024-12-08 17:38:35 +0100

    cmake renamed function parameter

--- cmake/Modules/TinyTestCommon.cmake
+++ cmake/Modules/TinyTestCommon.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
 # Configure a passed auto test
-function(tiny_configure_test name)
+function(tiny_configure_test target)
 
     set(options
         DEPENDS_ON_UNITTESTS INCLUDE_MIGRATIONS INCLUDE_MODELS PROVIDES_PCH RUN_SERIAL
@@ -14,9 +14,9 @@ ${TINY_UNPARSED_ARGUMENTS}")
     endif()
 
     # Configure PCH for the given test case
-    tiny_configure_test_pch(${name} ${TINY_PROVIDES_PCH})
+    tiny_configure_test_pch(${target} ${TINY_PROVIDES_PCH})
 
-    set_target_properties(${name}
+    set_target_properties(${target}
         PROPERTIES
             C_VISIBILITY_PRESET "hidden"
             CXX_VISIBILITY_PRESET "hidden"
@@ -26,11 +26,11 @@ ${TINY_UNPARSED_ARGUMENTS}")
 
     # These two settings allow to run tests in parallel using eg.: ctest --parallel 10
     # Parallel 30 saves ~12s on MSVC (from 42s to 30s)
-    set_tests_properties(${name} PROPERTIES RUN_SERIAL ${TINY_RUN_SERIAL})
+    set_tests_properties(${target} PROPERTIES RUN_SERIAL ${TINY_RUN_SERIAL})
 
     # Primarily to depend all functional tests on unit tests
     if(TINY_DEPENDS_ON_UNITTESTS)
-        set_tests_properties(${name}
+        set_tests_properties(${target}
             PROPERTIES
                 DEPENDS "databaseconnection;mysql_querybuilder;postgresql_querybuilder;\
 sqlite_querybuilder;blueprint;mysql_schemabuilder;postgresql_schemabuilder;\
@@ -41,13 +41,13 @@ sqlite_schemabuilder;mysql_tinybuilder"
     # Setup the correct PATH environment variable for the ctest command
     # Needed to find TinyOrm, TinyUtils, TinyDrivers, TinyMySql libraries in build tree
     if(TINY_TESTS_ENV_PATH)
-        set_property(TEST ${name} APPEND PROPERTY
+        set_property(TEST ${target} APPEND PROPERTY
             ENVIRONMENT_MODIFICATION "PATH=path_list_prepend:${TINY_TESTS_ENV_PATH}" # Quotes are needed because of \;
         )
     endif()
 
     if(MINGW)
-        target_compile_options(${name}
+        target_compile_options(${target}
             PRIVATE
                 $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:DEBUG>>:
                     -Wa,-mbig-obj
@@ -56,7 +56,7 @@ sqlite_schemabuilder;mysql_tinybuilder"
         )
     endif()
 
-    target_compile_definitions(${name}
+    target_compile_definitions(${target}
         PRIVATE
             PROJECT_TINYORM_TEST
             TINYORM_TESTS_CODE
@@ -68,7 +68,7 @@ sqlite_schemabuilder;mysql_tinybuilder"
 
     # Migrations header files
     if(TINY_INCLUDE_MIGRATIONS)
-        tiny_target_sources(${name} PRIVATE
+        tiny_target_sources(${target} PRIVATE
             PREFIX _tests_migration
             BASE_DIR "${PROJECT_SOURCE_DIR}/tests/database"
         )
@@ -76,17 +76,17 @@ sqlite_schemabuilder;mysql_tinybuilder"
 
     # Models header files
     if(TINY_INCLUDE_MODELS)
-        tiny_target_sources(${name} PRIVATE
+        tiny_target_sources(${target} PRIVATE
             PREFIX _models
             BASE_DIR "${PROJECT_SOURCE_DIR}/tests/models"
         )
     endif()
 
     if(NOT STRICT_MODE)
-        target_link_libraries(${name} PRIVATE ${TinyOrm_ns}::${CommonConfig_target})
+        target_link_libraries(${target} PRIVATE ${TinyOrm_ns}::${CommonConfig_target})
     endif()
 
-    target_link_libraries(${name}
+    target_link_libraries(${target}
         PRIVATE
             Qt${QT_VERSION_MAJOR}::Test
             ${TinyOrm_ns}::${TinyUtils_target}
@@ -106,7 +106,7 @@ sqlite_schemabuilder;mysql_tinybuilder"
         set(TinyRcFlagsToRemove "${TinyRcFlagsToRemove}" PARENT_SCOPE)
     endif()
 
-    tiny_resource_and_manifest(${name}
+    tiny_resource_and_manifest(${target}
         TEST RESOURCES_DIR "${PROJECT_SOURCE_DIR}/tests/${TINY_SOURCE_RESOURCES_DIR}/"
     )
 
@@ -116,11 +116,11 @@ endfunction()
 # The PROVIDES_PCH parameter from the tiny_configure_test() function tags a test case that
 # will provide PCH for all other test cases, only one test case can be tagged with it and
 # all other test cases will use this PCH without compilation.
-function(tiny_configure_test_pch name provides_pch)
+function(tiny_configure_test_pch target provides_pch)
 
     # Set at the beginning as this function can early return
     if(NOT CMAKE_DISABLE_PRECOMPILE_HEADERS)
-        target_compile_definitions(${name} PRIVATE TINYORM_USING_PCH)
+        target_compile_definitions(${target} PRIVATE TINYORM_USING_PCH)
     endif()
 
     # Qt <v6.9.0 breaks REUSE_FROM (will be fixed in Qt v6.9.0)
@@ -132,7 +132,7 @@ function(tiny_configure_test_pch name provides_pch)
             NOT (DEFINED ENV{TINY_QT6_TEST_TARGET_PATCHED} AND
                 "$ENV{TINY_QT6_TEST_TARGET_PATCHED}") # Quotes needed to avoid fail if undefined as conditions don't short-circuit!
     )
-        target_precompile_headers(${name} PRIVATE
+        target_precompile_headers(${target} PRIVATE
             $<$<COMPILE_LANGUAGE:CXX>:"${${TinyOrm_ns}_SOURCE_DIR}/include/pch.h">
         )
         return()
@@ -141,20 +141,20 @@ function(tiny_configure_test_pch name provides_pch)
     # Disable QT_TESTCASE_BUILDDIR/SOURCEDIR compile definitions for REUSE_FROM to work,
     # this property doesn't exist in my patched Qt6TestTargets.cmake
     if(TINY_QT_VERSION VERSION_GREATER_EQUAL "6.9.0")
-        set_target_properties(${name} PROPERTIES QT_SKIP_DEFAULT_TESTCASE_DIRS YES)
+        set_target_properties(${target} PROPERTIES QT_SKIP_DEFAULT_TESTCASE_DIRS YES)
     endif()
 
     if(provides_pch)
-        # Throw an exception if CACHE{TINY_TESTS_PCH_REUSE_FROM} isn't equal to the name
-        tiny_throw_if_wrong_reuse_from(${name})
+        # Throw an exception if CACHE{TINY_TESTS_PCH_REUSE_FROM} isn't equal to <target>
+        tiny_throw_if_wrong_reuse_from(${target})
 
-        target_precompile_headers(${name} PRIVATE
+        target_precompile_headers(${target} PRIVATE
             $<$<COMPILE_LANGUAGE:CXX>:"${${TinyOrm_ns}_SOURCE_DIR}/include/pch.h">
         )
 
-        set(TINY_TESTS_PCH_REUSE_FROM ${name} CACHE INTERNAL
-            "The name of the first test case that provides PCH for all other test cases \
-(used by REUSE_FROM).")
+        set(TINY_TESTS_PCH_REUSE_FROM ${target} CACHE INTERNAL
+            "The <target> name of the first test case that provides PCH for all other \
+test cases (used by REUSE_FROM).")
 
     else()
         # This should never happen :/
@@ -162,24 +162,24 @@ function(tiny_configure_test_pch name provides_pch)
         tiny_throw_if_no_cache_reuse_from()
 
         # TODO REUSE_FROM will fail if NOT $<COMPILE_LANGUAGE:CXX> silverqx
-        target_precompile_headers(${name} REUSE_FROM "$CACHE{TINY_TESTS_PCH_REUSE_FROM}")
+        target_precompile_headers(${target} REUSE_FROM "$CACHE{TINY_TESTS_PCH_REUSE_FROM}")
 
         return()
     endif()
 
 endfunction()
 
-# Throw an exception if the CACHE{TINY_TESTS_PCH_REUSE_FROM} is not equal to the name
-function(tiny_throw_if_wrong_reuse_from name)
+# Throw an exception if the CACHE{TINY_TESTS_PCH_REUSE_FROM} is not equal to the <target>
+function(tiny_throw_if_wrong_reuse_from target)
 
     # Nothing to do
     if(NOT DEFINED CACHE{TINY_TESTS_PCH_REUSE_FROM} OR
-            "$CACHE{TINY_TESTS_PCH_REUSE_FROM}" STREQUAL name # Quotes needed to avoid fail if undefined as conditions don't short-circuit!
+            "$CACHE{TINY_TESTS_PCH_REUSE_FROM}" STREQUAL target # Quotes needed to avoid fail if undefined as conditions don't short-circuit!
     )
         return()
     endif()
 
-    message(FATAL_ERROR "The '${name}' test case can't set the PROVIDES_PCH because \
+    message(FATAL_ERROR "The '${target}' test case can't set the PROVIDES_PCH because \
 the \$CACHE{TINY_TESTS_PCH_REUSE_FROM} is already set \
 for the '$CACHE{TINY_TESTS_PCH_REUSE_FROM}' test case, in ${CMAKE_CURRENT_FUNCTION}().")
 
