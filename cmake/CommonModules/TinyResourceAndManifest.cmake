e6bc3a01ecdba2dff2c133fa4a5251a6bc4f2757
silverqx <silver.zachara@gmail.com>
2024-12-13 16:44:14 +0100

    cmake used cmake_path(ABSOLUTE_PATH NORMALIZE)
    Used instead of manual concatenation with the file(TO_CMAKE_PATH)
    command.
    
     - extracted code to private functions/macros
     - removed trailing slashes
     - enhanced logic
     - quotes string
     - added/updated comments

--- cmake/CommonModules/TinyResourceAndManifest.cmake
+++ cmake/CommonModules/TinyResourceAndManifest.cmake
@@ -47,14 +47,9 @@ path (must be relative to CMAKE_CURRENT_BINARY_DIR) in ${CMAKE_CURRENT_FUNCTION}
     # Include Windows RC and manifest file for a shared library or executable
     get_target_property(targetType ${target} TYPE)
 
+    # Return if the target type isn't an executable (is static archive library)
     # The static archive doesn't need an RC or manifest file
-    if(NOT CMAKE_SYSTEM_NAME STREQUAL "Windows" OR
-            NOT (targetType STREQUAL "EXECUTABLE" OR
-                 targetType STREQUAL "SHARED_LIBRARY" OR
-                 targetType STREQUAL "MODULE_LIBRARY")
-    )
-        return()
-    endif()
+    _tiny_resource_return_if_wrong_type() # return()
 
     # Initialize variables
 
@@ -62,55 +57,20 @@ path (must be relative to CMAKE_CURRENT_BINARY_DIR) in ${CMAKE_CURRENT_FUNCTION}
     # if a folder doesn't exist. Another problem is that it resolves symlinks to actual
     # locations, and that's undesirable with QtCreator's build tree junctions feature.
 
-    # TINY_OUTPUT_DIR (to absolute path)
-    if("${TINY_OUTPUT_DIR}" STREQUAL "")
-        set(TINY_OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/${TINY_BUILD_TMP_DIR}/")
-    else() # It's always relative, see the check above
-        string(PREPEND TINY_OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/")
-    endif()
+    # TINY_OUTPUT_DIR as an absolute path, normalize it, and remove trailing slashes
+    _tiny_resource_get_output_dir(TINY_OUTPUT_DIR)
+    # TINY_RESOURCES_DIR as an absolute path, normalize it, and remove trailing slashes
+    _tiny_resource_get_resources_dir(TINY_RESOURCES_DIR)
 
-    # TINY_RESOURCES_DIR (to absolute path)
-    if("${TINY_RESOURCES_DIR}" STREQUAL "")
-        set(TINY_RESOURCES_DIR "${CMAKE_CURRENT_SOURCE_DIR}/${TINY_SOURCE_RESOURCES_DIR}")
-    elseif(NOT IS_ABSOLUTE "${TINY_RESOURCES_DIR}")
-        string(PREPEND TINY_RESOURCES_DIR "${CMAKE_CURRENT_SOURCE_DIR}/")
-    endif()
-
-    file(TO_CMAKE_PATH ${TINY_OUTPUT_DIR} TINY_OUTPUT_DIR)
-    file(TO_CMAKE_PATH ${TINY_RESOURCES_DIR} TINY_RESOURCES_DIR)
+    # Prepare file basename for the TinyXyz.rc.in resource file
+    _tiny_resource_prepare_resource_basename()
+    # Prepare file basename for the TinyXyz.exe/dll.manifest file
+    _tiny_resource_prepare_manifest_basename()
 
     # Modify the Tom_target variable for substitution (original exe and icon filename)
     # This is special logic because Tom_target is used in 3 CMake projects
     # Used only in the tom.rc.in, I'm not going to wrap it in the if()
     set(Tom_target ${target})
-
-    # Allow passing custom RC basename
-    if(NOT "${TINY_RESOURCE_BASENAME}" STREQUAL "")
-        set(rcBasename ${TINY_RESOURCE_BASENAME})
-
-    # All tests use the same TinyTest.rc.in file
-    elseif(TINY_TEST)
-        set(rcBasename TinyTest)
-
-        # Used for icon basename
-        set(TinyTest_icon ${rcBasename})
-        # Test's RC file has a common substitution token for all tests
-        set(TinyTest_target ${target})
-    else()
-        set(rcBasename ${target})
-    endif()
-
-    # Allow passing custom manifest basename
-    if(NOT "${TINY_MANIFEST_BASENAME}" STREQUAL "")
-        set(manifestBasename ${TINY_MANIFEST_BASENAME})
-        # For MinGW (used only in the tom.rc.in)
-        set(Tom_manifest ${TINY_MANIFEST_BASENAME})
-    else()
-        set(manifestBasename ${rcBasename})
-        # For MinGW (used only in the tom.rc.in)
-        set(Tom_manifest ${rcBasename})
-    endif()
-
     # CMake doesn't have problem with UTF-8 encoded files
     set(pragma_codepage "65001")
 
@@ -155,3 +115,106 @@ path (must be relative to CMAKE_CURRENT_BINARY_DIR) in ${CMAKE_CURRENT_FUNCTION}
     endif()
 
 endfunction()
+
+# Return if the target type isn't an executable (is static archive library)
+macro(_tiny_resource_return_if_wrong_type)
+
+    # The static archive doesn't need an RC or manifest file
+    if(NOT CMAKE_SYSTEM_NAME STREQUAL "Windows" OR
+            NOT (targetType MATCHES "^(EXECUTABLE|SHARED_LIBRARY|MODULE_LIBRARY)$")
+    )
+        return() # Applies for the caller, not here
+    endif()
+
+endmacro()
+
+# Get TINY_OUTPUT_DIR as an absolute path, normalize it, and remove trailing slashes
+function(_tiny_resource_get_output_dir output_dir_variable)
+
+    # Provide the default value if empty, undefined, or missing value
+    if("${${output_dir_variable}}" STREQUAL "")
+        set(${output_dir_variable}
+            "${CMAKE_CURRENT_BINARY_DIR}/${TINY_BUILD_TMP_DIR}" PARENT_SCOPE
+        )
+        return()
+    endif()
+
+    # It's always relative, see the check above (so no if() checks needed)
+    cmake_path(ABSOLUTE_PATH ${output_dir_variable}
+        BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+        NORMALIZE OUTPUT_VARIABLE result
+    )
+
+    # Remove trailing slashes
+    tiny_path_remove_slashes(result)
+
+    set(${output_dir_variable} "${result}" PARENT_SCOPE)
+
+endfunction()
+
+# Get TINY_RESOURCES_DIR as an absolute path, normalize it, and remove trailing slashes
+function(_tiny_resource_get_resources_dir resources_dir_variable)
+
+    # For a shorter variable name
+    set(result "${${resources_dir_variable}}")
+
+    # Provide the default value if empty, undefined, or missing value
+    if("${result}" STREQUAL "")
+        set(${resources_dir_variable}
+            "${CMAKE_CURRENT_SOURCE_DIR}/${TINY_SOURCE_RESOURCES_DIR}" PARENT_SCOPE
+        )
+        return()
+    endif()
+
+    # Absolute paths stay untouched
+    cmake_path(ABSOLUTE_PATH result NORMALIZE OUTPUT_VARIABLE result) # Relative to CMAKE_CURRENT_SOURCE_DIR
+
+    # Remove trailing slashes
+    tiny_path_remove_slashes(result)
+
+    set(${resources_dir_variable} "${result}" PARENT_SCOPE)
+
+endfunction()
+
+# Prepare file basename for the TinyXyz.rc.in resource file
+macro(_tiny_resource_prepare_resource_basename)
+
+    # if()-s order is important in this macro
+
+    # Set what was passed
+    if(NOT "${TINY_RESOURCE_BASENAME}" STREQUAL "")
+        set(rcBasename ${TINY_RESOURCE_BASENAME})
+
+    # All tests use the same TinyTest.rc.in file
+    elseif(TINY_TEST)
+        set(rcBasename "TinyTest")
+
+        # Used for icon basename
+        set(TinyTest_icon ${rcBasename})
+        # Test's RC file has a common substitution token for all tests
+        set(TinyTest_target ${target})
+
+    # Provide the default value if empty, undefined, or missing value
+    else()
+        set(rcBasename ${target})
+    endif()
+
+endmacro()
+
+# Prepare file basename for the TinyXyz.exe/dll.manifest file
+macro(_tiny_resource_prepare_manifest_basename)
+
+    # Provide the default value if empty, undefined, or missing value
+    if("${TINY_MANIFEST_BASENAME}" STREQUAL "")
+        set(manifestBasename ${rcBasename})
+        # For MinGW (used only in the tom.rc.in)
+        set(Tom_manifest ${rcBasename})
+
+    # Otherwise set what was passed
+    else()
+        set(manifestBasename ${TINY_MANIFEST_BASENAME})
+        # For MinGW (used only in the tom.rc.in)
+        set(Tom_manifest ${TINY_MANIFEST_BASENAME})
+    endif()
+
+endmacro()
